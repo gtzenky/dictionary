2465d392dd7a91d9179e262d80dc489dbe43f294
Thong Nguyen <thongmgnguyen@kms-technology.com>
2014-11-30 12:28:46 +0700

    read ld2

--- src/com/dictionary/dic/MainActivity.java
+++ src/com/dictionary/dic/MainActivity.java
@@ -1,17 +1,29 @@
 package com.dictionary.dic;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import com.ldx.parser.LingoesLd2Reader;
 
 
 public class MainActivity extends Activity {
+  
+  public static String SEARCH_FIELD = "com.dictionary.dic.MainActivity.search_text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
@@ -28,9 +40,27 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        switch (id) {
+          case R.id.action_search:
             return true;
+  
+          case R.id.action_settings:
+            return true;
+          
+          default:
+            return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+    }
+    
+    public void sendMessage(View view) throws IOException {
+      
+      String ld2File = Environment.getExternalStorageDirectory().getAbsolutePath() + "/e.ld2";
+      LingoesLd2Reader a = new LingoesLd2Reader();
+      a.abc(ld2File);
+      Intent intent = new Intent(this, DisplayMessageActivity.class);
+      EditText searchText = (EditText) findViewById(R.id.search_message);
+      String searchMessage = searchText.getText().toString();
+      intent.putExtra(SEARCH_FIELD, searchMessage);
+      startActivity(intent);
     }
 }
